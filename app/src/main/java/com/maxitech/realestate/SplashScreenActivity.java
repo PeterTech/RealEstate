c23be4e31d99cb5c8aa5370db5b581a2230efcb3
bitfield66 <vinayyam@gmail.com>
2016-10-14 23:28:36 +0530

    test

---
+++ app/src/main/java/com/maxitech/realestate/SplashScreenActivity.java
@@ -0,0 +1,12 @@
+package com.maxitech.realestate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends BaseActivity {
+
+    @Override
+    public void initial() {
+
+    }
+}
