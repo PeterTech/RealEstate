9b1fe96c8fcf538b442c6429ee9b87f531ad08f5
PeterTech <prabhati5vssut@gmail.com>
2016-10-15 12:41:26 +0530

    ini

--- app/src/main/java/com/maxitech/realestate/SplashScreenActivity.java
+++ app/src/main/java/com/maxitech/realestate/SplashScreenActivity.java
@@ -1,12 +1,33 @@
 package com.maxitech.realestate;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SplashScreenActivity extends BaseActivity {
-
+    private LinearLayout ll_Body;
     @Override
     public void initial() {
+        ll_Body=(LinearLayout)inflater.inflate(R.layout.splash_screen,null);
+        llHeader.setVisibility(View.GONE);
+        llMiddle.addView(ll_Body, ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                finish();
+                Intent intent		=	new Intent(SplashScreenActivity.this, LoginActivity.class);//CompetitorsListActivity
+                startActivity(intent);
+            }
+
+
+        },2500);
+
 
     }
 }
