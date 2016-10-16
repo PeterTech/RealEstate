896074ea8adf00e1781fcb21b25fa04e04810d2e
bitfield66 <vinayyam@gmail.com>
2016-10-16 15:13:10 +0530

    test

--- app/src/main/java/com/maxitech/realestate/PropertiesListActivity.java
+++ app/src/main/java/com/maxitech/realestate/PropertiesListActivity.java
@@ -1,7 +1,6 @@
 package com.maxitech.realestate;
 
 import android.app.ActionBar;
-import android.graphics.Movie;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
